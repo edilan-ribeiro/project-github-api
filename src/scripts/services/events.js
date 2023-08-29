2197573815fff8b9fb4daee0380b94cd3d08faca
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-08-28 22:33:22 -0300

    some space removed

--- src/scripts/services/events.js
+++ src/scripts/services/events.js
@@ -1,6 +1,5 @@
 import { baseUrl } from "./variables.js"
 
-
 async function getEvents(userName) {
     const response = await fetch(`${baseUrl}/${userName}/events`)
     return await response.json()
